9ddcc12e2eb85b1138280bd863f5c7f92bb8da10
Rob Moore <giftiger.wunsch@gmail.com>
2024-09-25 08:43:32 +0100

    Add python sample code + tests
    Code stolen from my giftig/s3-browser project
    
    Tweak the import regex a bit as it was failing to find single python
    imports: that's because it was expecting a space directly after import,
    and then either a dotted path, import block, or additional space
    directly before the symbol. In a simple case that'd require two spaces,
    which isn't correct. Fix this by simply removing the space after the
    word import; it's still going to be looking for a block or dotted path
    next anyway.
    
    This adjustment technically means it might incorrectly match a string
    like "import.foo.bar.baz", but that seems unlikely to come up.

--- src/search/tests.rs
+++ src/search/tests.rs
@@ -3,11 +3,16 @@ use super::*;
 use crate::args::SearchMode;
 
 /// Prepend the prefix to the scala sample files for brevity
-fn filename(s: &str) -> String {
+fn scala_file(s: &str) -> String {
     format!(
         "test/fixtures/scala/src/main/scala/com/xantoria/mmo/common/{}",
         s
     )
+}
+
+/// Prepend the prefix to the python sample files for brevity
+fn py_file(s: &str) -> String {
+    format!("test/fixtures/python/{}", s)
 }
 
 #[test]
@@ -17,7 +22,7 @@ fn search_scala_trait() {
     let search = Search::new(&SearchMode::Class);
     let expected = vec![Hit {
         term: "Update".to_string(),
-        filename: filename("model/updates/Update.scala"),
+        filename: scala_file("model/updates/Update.scala"),
         line: Some(3),
         col: Some(1),
         text: "trait Update".to_string(),
@@ -36,7 +41,7 @@ fn search_scala_trait_with_companion() {
     let expected = vec![
         Hit {
             term: "InventoryUpdate".to_string(),
-            filename: filename("model/updates/InventoryUpdate.scala"),
+            filename: scala_file("model/updates/InventoryUpdate.scala"),
             line: Some(5),
             col: Some(8),
             text: "sealed trait InventoryUpdate extends Update {".to_string(),
@@ -44,7 +49,7 @@ fn search_scala_trait_with_companion() {
         },
         Hit {
             term: "InventoryUpdate".to_string(),
-            filename: filename("model/updates/InventoryUpdate.scala"),
+            filename: scala_file("model/updates/InventoryUpdate.scala"),
             line: Some(11),
             col: Some(1),
             text: "object InventoryUpdate {".to_string(),
@@ -58,12 +63,12 @@ fn search_scala_trait_with_companion() {
 }
 
 #[test]
-/// Find definitions of the InventoryUpdate trait + companion object
+/// Find a simple, single import
 fn search_scala_import_single() {
     let search = Search::new(&SearchMode::Import);
     let expected = vec![Hit {
         term: "InventoryUpdate".to_string(),
-        filename: filename("updates/inventory/InventoryUpdateResult.scala"),
+        filename: scala_file("updates/inventory/InventoryUpdateResult.scala"),
         line: Some(5),
         col: Some(1),
         text: "import com.xantoria.mmo.common.model.updates.InventoryUpdate".to_string(),
@@ -76,12 +81,12 @@ fn search_scala_import_single() {
 }
 
 #[test]
-/// Find definitions of the InventoryUpdate trait + companion object
+/// Make sure imports can be found in a multi-import
 fn search_scala_import_multi() {
     let search = Search::new(&SearchMode::Import);
     let expected = vec![Hit {
         term: "Future".to_string(),
-        filename: filename("model/updates/StatSheetUpdate.scala"),
+        filename: scala_file("model/updates/StatSheetUpdate.scala"),
         line: Some(4),
         col: Some(1),
         text: "import scala.concurrent.{ExecutionContext, Future}".to_string(),
@@ -99,7 +104,7 @@ fn search_scala_def() {
     let search = Search::new(&SearchMode::Function);
     let expected = vec![Hit {
         term: "toString".to_string(),
-        filename: filename("model/updates/StatSheetUpdate.scala"),
+        filename: scala_file("model/updates/StatSheetUpdate.scala"),
         line: Some(25),
         col: Some(14),
         text: "    override def toString: String = {".to_string(),
@@ -112,24 +117,142 @@ fn search_scala_def() {
 }
 
 #[test]
+/// Find a python class which extends Exception
+fn search_python_class_extending_parent() {
+    let search = Search::new(&SearchMode::Class);
+    let expected = vec![Hit {
+        term: "TokeniserException".to_string(),
+        filename: py_file("tokeniser.py"),
+        line: Some(103),
+        col: Some(1),
+        text: "class TokeniserException(Exception):".to_string(),
+        lang: DetectedLanguage::Python,
+    }];
+
+    let actual = search.search("TokeniserException").unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+/// Find new-style python class which doesn't extend anything
+/// N.B. also ensures it rules out classes with a substring of the term
+fn search_python_class_plain() {
+    let search = Search::new(&SearchMode::Class);
+    let expected = vec![Hit {
+        term: "Cli".to_string(),
+        filename: py_file("cli.py"),
+        line: Some(17),
+        col: Some(1),
+        text: "class Cli:".to_string(),
+        lang: DetectedLanguage::Python,
+    }];
+
+    let actual = search.search("Cli").unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn search_python_def() {
+    let search = Search::new(&SearchMode::Function);
+    let expected = vec![Hit {
+        term: "add_bookmark".to_string(),
+        filename: py_file("cli.py"),
+        line: Some(197),
+        col: Some(5),
+        text: "    def add_bookmark(self, name, path):".to_string(),
+        lang: DetectedLanguage::Python,
+    }];
+
+    let actual = search.search("add_bookmark").unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+/// Find a simple, single python import
+fn search_python_import_single() {
+    let search = Search::new(&SearchMode::Import);
+    let expected = vec![
+        Hit {
+            term: "readline".to_string(),
+            filename: py_file("cli.py"),
+            line: Some(6),
+            col: Some(1),
+            text: "import readline".to_string(),
+            lang: DetectedLanguage::Python,
+        },
+        Hit {
+            term: "readline".to_string(),
+            filename: py_file("completion.py"),
+            line: Some(3),
+            col: Some(1),
+            text: "import readline".to_string(),
+            lang: DetectedLanguage::Python,
+        },
+    ];
+
+    let actual = search.search("readline").unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+/// Find a python import within a multi-import
+fn search_python_import_multi() {
+    let search = Search::new(&SearchMode::Import);
+    let expected = vec![Hit {
+        term: "tokeniser".to_string(),
+        filename: py_file("cli.py"),
+        line: Some(11),
+        col: Some(17),
+        text: "from s3_browser import bookmarks, client, completion, paths, tokeniser, utils".to_string(),
+        lang: DetectedLanguage::Python,
+    }];
+
+    let actual = search.search("tokeniser").unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+/// Make sure imports are found even if they've been renamed while importing
+fn search_python_import_renamed() {
+    let search = Search::new(&SearchMode::Import);
+    let expected = vec![Hit {
+        term: "ArgumentParser".to_string(),
+        filename: py_file("cli.py"),
+        line: Some(12),
+        col: Some(26),
+        text: "from s3_browser.argparse import ArgumentParser as SafeParser".to_string(),
+        lang: DetectedLanguage::Python,
+    }];
+
+    let actual = search.search("ArgumentParser").unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
 /// Find filenames which are a partial match for the search term
 fn search_partial_filename() {
     let search = Search::new(&SearchMode::File);
     let expected = vec![
         Hit {
             term: "InventoryUpdate".to_string(),
-            filename: filename("updates/inventory/InventoryUpdateResult.scala"),
+            filename: scala_file("updates/inventory/InventoryUpdateResult.scala"),
             line: None,
             col: None,
-            text: filename("updates/inventory/InventoryUpdateResult.scala"),
+            text: scala_file("updates/inventory/InventoryUpdateResult.scala"),
             lang: DetectedLanguage::Scala,
         },
         Hit {
             term: "InventoryUpdate".to_string(),
-            filename: filename("model/updates/InventoryUpdate.scala"),
+            filename: scala_file("model/updates/InventoryUpdate.scala"),
             line: None,
             col: None,
-            text: filename("model/updates/InventoryUpdate.scala"),
+            text: scala_file("model/updates/InventoryUpdate.scala"),
             lang: DetectedLanguage::Scala,
         },
     ];
